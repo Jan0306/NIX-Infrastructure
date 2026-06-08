ab2c250223ee13851867773698982f410729dc74
Jan0306 <jan.robbers@icloud.com>
2026-06-08 22:06:32 +0200

    Refactor configuration files: streamline system packages and user settings

--- modules/common.nix
+++ modules/common.nix
@@ -3,4 +3,36 @@
 {
  time.timeZone = "Europe/Berlin";
  services.openssh.enable = true;
+
+ networking.networkmanager.enable = true;
+
+ i18n.defaultLocale = "de_DE.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+   git
+  ];
 }
--- modules/users.nix
+++ modules/users.nix
@@ -3,7 +3,9 @@
 {
  users.users.jan = {
   isNormalUser = true;
-  extraGroups = [ "wheel" ];
+  description = "Jan";
+  extraGroups = [ "networkmanager" "wheel" ];
+  packages = with pkgs; [];
   openssh.authorizedKeys.keys = [
    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGWN+/adzSEbOxFsIiGQOsfiPhFoiH+5gUj34KTZ2QpB janrobbers@MacBook-Pro-von-Jan.fritz.box"
   ];
